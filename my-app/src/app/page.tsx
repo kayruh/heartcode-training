1deca350fe44094d86a1e364650c9fdf2354c2e4
keira ashley <keira@Keiras-MacBook-Pro.local>
2024-10-08 13:27:37 +0800

    with updated image

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,7 +6,7 @@ import drugpic from "src/app/assets/drug-abuse-cycle.webp"
 export default function Home() {
   return (<div> Hello World
     Drug abuse, or substance abuse, is defined as the use of a drug in amounts or methods that is harmful to the individual or others.
-    <Image src={{drugpic}}/>
+    <Image src={drugpic}/>
     
   </div>);
 }
