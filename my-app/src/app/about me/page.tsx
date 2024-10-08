12ef3b45a4950e6a31c72d9999780be29b74cf9c
keira ashley <keira@Keiras-MacBook-Pro.local>
2024-10-08 10:43:13 +0800

    about me and quiz

---
+++ my-app/src/app/about me/page.tsx
@@ -0,0 +1,3 @@
+export default function AboutMe() {
+    return (<div> Hello World this is the about me route</div>);
+  }
---
+++ my-app/src/app/quiz/page.tsx
@@ -0,0 +1,3 @@
+export default function Quiz() {
+    return (<div> Hello World this is the quiz route</div>);
+  }
